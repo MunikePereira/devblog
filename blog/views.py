6013fe040996922d570f941f51ddabea2a432b23
MUNIKE NICOLE PEREIRA LIMA <05432033011@senacrs.edu.br>
2026-06-25 20:41:34 -0300

    criamos uma view e uma rotas sobre o projeto - SLIDE 66

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    mensagem = "<h1> Bem-vindo ao DevBLog</h1> <p> Em breve, artigos aqui. </p>"
+
+    return HttpResponse(mensagem)
+
+def sobre_nos(request):
+    mensagem = "<h1> Sobre o DevBlog </h1> <p> Somos um canal online para você ficar por dentro das noticias do mundo tecnologico </p>"
+    return HttpResponse(mensagem)
+

04657b7f3f517eafbaa6c3631107ad2fcd734a73
MUNIKE NICOLE PEREIRA LIMA <05432033011@senacrs.edu.br>
2026-06-29 20:26:01 -0300

    criamos um arquivo base html e atualizamos nossa view nele  - SLIDE 67

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,7 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
 def home(request):
-    mensagem = "<h1> Bem-vindo ao DevBLog</h1> <p> Em breve, artigos aqui. </p>"
-
-    return HttpResponse(mensagem)
+    return render(request, "blog/index.html")
 
 def sobre_nos(request):
     mensagem = "<h1> Sobre o DevBlog </h1> <p> Somos um canal online para você ficar por dentro das noticias do mundo tecnologico </p>"

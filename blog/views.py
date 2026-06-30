a0f11c37a38398d474fc084622ed3e1d24aa1819
MUNIKE NICOLE PEREIRA LIMA <05432033011@senacrs.edu.br>
2026-06-29 21:00:59 -0300

    excercico para criar o sobre da pagina  - SLIDE 67

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,5 @@ def home(request):
     return render(request, "blog/index.html")
 
 def sobre_nos(request):
-    mensagem = "<h1> Sobre o DevBlog </h1> <p> Somos um canal online para você ficar por dentro das noticias do mundo tecnologico </p>"
-    return HttpResponse(mensagem)
+    return render(request, "blog/sobre.html")
 

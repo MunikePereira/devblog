b30b9054b611de61ac1300f06819ae019f69c219
MUNIKE NICOLE PEREIRA LIMA <05432033011@senacrs.edu.br>
2026-07-02 19:52:38 -0300

    checkpoint 7 criar o botao de listar categorias  - SLIDE 71

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,22 @@
-from django.http import HttpResponse
+from django.http import HttpResponse,request
 from django.shortcuts import render
-from .models import Artigo
+from .models import Artigo, Categoria
 
-def home(request):
-    noticias = Artigo.objects.all()
+def home(request, categoria_id=None):
+    categoria_id = request.GET.get('categoria')
+
+    if categoria_id:
+        noticias = Artigo.objects.filter(categoria_id=categoria_id)
+    else:
+        noticias = Artigo.objects.all()
+
+    categorias = Categoria.objects.all()
     contexto = {
-        'lista_artigos' : noticias
+        'lista_artigos' : noticias,
+        'lista_categorias' : categorias,
     }
+
+
     return render(request, "blog/index.html", contexto)
 
 def sobre_nos(request):

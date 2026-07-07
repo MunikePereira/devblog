73b5daa6ef33b901fdf3a065e7734c86670bb6bd
MUNIKE NICOLE PEREIRA LIMA <05432033011@senacrs.edu.br>
2026-07-07 20:13:10 -0300

    criando  autenticações de segurança para o metetodo post e instalando o djangorestframework-simplejwt         - SLIDE 76

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,9 @@ from .forms import ContatoForm
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializeres import ArtigoSerializer, CategoriaSerializer
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 def home(request):
     categoria_selecionadas = request.GET.get('categoria')
@@ -68,4 +71,14 @@ def api_listar_categoria(request):
     categorias = Categoria.objects.all()
     serializer = CategoriaSerializer(categorias, many=True)
     return Response(serializer.data)
-    
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def api_criar_artigo(request):
+    serializer = ArtigoSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=201)
+    return Response(serializer.errors, status=400)
